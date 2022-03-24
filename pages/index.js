43acefc86c30319e8c2eefc946e2fd3b63790c06
tahaahmed <tahaahmedanees2@gmail.com>
2022-03-24 21:47:31 +0500

    2 Change

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,135 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import { DatePicker } from "antd";
+import { DatePicker, Button, Form, Input, Checkbox, Carousel } from "antd";
 
 export default function Home() {
+  const Click = () => {
+    console.log("click");
+  };
+
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
   return (
-    <div className="grid mt-5 bg-red-400 rounded p-4 m-4 items-center justify-center">
-      <h1>Hello World</h1>
-      <h1>Taha Ahmmed</h1>
-      <DatePicker />
+    <div className="grid mt-5 items-center justify-center">
+      <Carousel autoplay>
+        <div
+          className="text-black"
+          style={{
+            height: "160px",
+            color: "#000",
+            lineHeight: "160px",
+            textAlign: "center",
+            background: "green",
+          }}
+        >
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+        </div>
+      </Carousel>
+      <Form
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="on"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Please input username",
+            },
+          ]}
+        >
+          <Input />
+          <br />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input  password!",
+            },
+          ]}
+        >
+          <Input.Password />
+          <br />
+        </Form.Item>
+
+        <Form.Item
+          name="datepicker"
+          valuePropName="datepicker"
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <DatePicker
+            rules={[
+              {
+                required: true,
+                message: "Please select date",
+              },
+            ]}
+          />
+        </Form.Item>
+
+        <Form.Item
+          name="remember"
+          valuePropName="checked"
+          wrapperCol={{
+            offset: 12,
+            span: 16,
+          }}
+        >
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button
+            type="danger"
+            htmlType="submit"
+            className="text-black hover:text-green"
+            onClick={Click}
+          >
+            Press me
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 }
